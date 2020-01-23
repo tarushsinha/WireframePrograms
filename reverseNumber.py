587457f396a14b33b68ab00b509132d937cb79a5
Tarush Sinha <tarush.sinha@gmail.com>
2020-01-22 16:12:20 -0800

    better technique for reversal

--- reverseNumber.py
+++ reverseNumber.py
@@ -5,3 +5,14 @@ def reverseNumber(inp):
     for i in ret:
         v = v + i
     return int(v)
+
+## same as above method, works without relying on List.reverse()
+def independentReverse(inp):
+    ret = list(str(inp))
+    v = ""
+    for i in ret[::-1]:
+        v = v + i
+    return int(v)
+
+print(reverseNumber(81))
+print(independentReverse(81))
